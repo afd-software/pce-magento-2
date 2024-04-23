afac2cd218ab277befc811fd79b3438303038ca7
Svetlana <svetlana.vasileva@afd.co.uk>
2024-04-23 14:57:01 +0000

    1.6.14

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -1,6 +1,6 @@
 var config = {
     paths: {
-            'afdPce': 'Afd_Pce/js/afd.jquery.1.11.0.5.min',
+            'afdPce': 'Afd_Pce/js/afd.jquery.1.11.3.min',
             'afdPcePostRender': 'Afd_Pce/js/afd.post.render.jquery.1.11.0.5.min'
     },
     shim: {
