dfd7f36ee1879173792fb16a3a372ddc5cff86e0
Matthew Dolman <matthew.dolman@afd.co.uk>
2025-12-05 18:43:49 +0800

    remove duplicate files

--- Block/Block/AdminSalesBilling.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Afd\Pce\Block;
-
-class AfdAdminSalesBilling extends \Magento\Sales\Block\Adminhtml\Order\Address\Form
-{
-    protected $_template = 'Afd_Pce::admin_order_billing.phtml';
-
-}
--- view/view/adminhtml/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line
-var config = {
-  map: {
-    '*': {
-      'Magento_Sales/order/create/form': 'Afd_Pce/order/create/form'
-    }
-  }
-}
